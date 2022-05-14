6ad42705db74f2b19dd0254dab5aedfcce577a94
Skronkie <ebrockway01@gmail.com>
2022-05-14 16:44:04 -0500

    initializing files

---
+++ src/Nav/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <header>
+      <h2>Ethan Brockway</h2>
+      <nav>
+        <ul>
+          <li>About me</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+          <li>Resume</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Nav;
